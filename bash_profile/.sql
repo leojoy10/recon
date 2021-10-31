f318e5fd309254c490429553d5dbc3009f27b014
Sam Joy <64733282+leojoy10@users.noreply.github.com>
2021-10-31 17:04:38 +0530

    Add files via upload

---
+++ bash_profile/.sql
@@ -0,0 +1,24 @@
+#sql-----------------------------------------------------------------------------------------
+
+
+
+
+
+sql(){
+
+mkdir results/sql
+
+#Exploit
+python3 ~/tools/sqlmap-dev/sqlmap.py -m sql.txt --batch | tee sql-result
+
+#move
+mv sql-result results/sql
+
+}
+
+
+
+
+
+#sql-----------------------------------------------------------------------------------------
+
